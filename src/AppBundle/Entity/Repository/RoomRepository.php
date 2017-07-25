dd8613fc6ebd0527cfd1ed8f995aeeec1df507af
looscolin <loos_colin@web.de>
2017-07-25 10:16:22 +0200

    added RoomRepository and ComponentTypeRepositories

---
+++ src/AppBundle/Entity/Repository/RoomRepository.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Colin
+ * Date: 25.07.2017
+ * Time: 09:21
+ */
+
+namespace AppBundle\Entity\Repository;
+
+
+use AppBundle\Entity\ManagedConnection;
+use AppBundle\Entity\Room;
+
+class RoomRepository
+{
+    /**
+     * @return array
+     */
+    public static function getAllRooms()
+    {
+        $managedConnection = new ManagedConnection();
+        $connection = $managedConnection->getConnection();
+
+        $query = "SELECT * FROM raeume;";
+        $result = $connection->query($query);
+
+        $rooms = [];
+
+        while ($row = $result->fetch_assoc())
+        {
+            $room = new Room();
+            $room->setId($row["r_id"]);
+            $room->setNumber($row["r_nr"]);
+            $room->setDescription($row["r_bezeichnung"]);
+            $room->setNote($row["r_notiz"]);
+            $rooms[] = $room;
+        }
+
+        if($connection->error)
+        {
+            throw new \Exception("Selektieren der Räume fehlgeschlagen");
+        }
+
+        return $rooms;
+    }
+
+    /**
+     * @param $id
+     * @return Room
+     */
+    public static function getRoomById($id)
+    {
+        $managedConnection = new ManagedConnection();
+        $connection = $managedConnection->getConnection();
+
+        $query = $connection->prepare("SELECT * FROM raeume WHERE r_id = ?;");
+        $query->bind_param("i", $id);
+        $query->execute();
+
+        $result = $connection->query($query);
+
+        $row = $result->fetch_row();
+
+        $room = new Room();
+        $room->setId($row["r_id"]);
+        $room->setNumber($row["r_nr"]);
+        $room->setDescription($row["r_bezeichnung"]);
+        $room->setNote($row["r_notiz"]);
+
+        if($connection->error)
+        {
+            throw new \Exception("Selektieren des Raumes fehlgeschlagen");
+        }
+
+        return $room;
+    }
+
+    public static function createRoom(Room $room)
+    {
+        $managedConnection = new ManagedConnection();
+        $connection = $managedConnection->getConnection();
+
+        $query = $connection->prepare("INSERT INTO raeume(r_nr, r_bezeichnung, r_notiz) VALUES (?, ?, ?);");
+        $query->bind_param("sss", $room->getId(), $room->getNumber(), $room->getDescription(), $room->getNote());
+        $query->execute();
+
+        $connection->query($query);
+
+        if($connection->error)
+        {
+            throw new \Exception("Erstellung des Raumes fehlgeschlagen");
+        }
+    }
+
+    /**
+     * @param Room $room
+     * @throws \Exception
+     */
+    public static function updateRoom(Room $room)
+    {
+        $managedConnection = new ManagedConnection();
+        $connection = $managedConnection->getConnection();
+
+        $query = $connection->prepare("UPDATE raeume SET r_nr = ?, r_bezeichnung = ?, r_notiz = ? WHERE r_id = ?;");
+        $query->bind_param("sssi", $room->getNumber(), $room->getDescription(), $room->getNote(), $room->getId());
+        $query->execute();
+
+        $connection->query($query);
+
+        if($connection->error)
+        {
+            throw new \Exception("Ändern des Raumes fehlgeschlagen");
+        }
+    }
+}
