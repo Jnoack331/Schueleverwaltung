f8505413a17d5353ed1ecbe3849b1d019189307d
obihoernchenxD <codhater2000@gmail.com>
2017-07-26 10:22:39 +0200

    meddl julius

--- src/AppBundle/Controller/RoomController.php
+++ src/AppBundle/Controller/RoomController.php
@@ -9,8 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 
-class RoomController extends Controller
-{
+class RoomController extends AbstractController {
     /**
      * Fetches all Rooms from the database and renders a template to display them
      *
@@ -20,9 +19,7 @@ class RoomController extends Controller
         try {
             $rooms = RoomRepository::getAllRooms();
         } catch (Exception $e) {
-            return $this->render("room/list.html.twig", [
-               "message" => "Fehler beim Laden der Räume"
-            ]);
+            return $this->renderError("room_view", $e);
         }
         return $this->render("room/list.html.twig", ["rooms" => $rooms]);
     }
@@ -45,9 +42,7 @@ class RoomController extends Controller
             try {
                 $id = RoomRepository::createRoom($room);
             } catch (Exception $e) {
-                return $this->render("room/list.html.twig", [
-                    "message" => "Fehler beim Erstellen des Raums"
-                ]);
+                return $this->renderError("room_create", $e);
             }
 
             return $this->redirectToRoute("room_detail", ["id" => $id]);
@@ -65,9 +60,7 @@ class RoomController extends Controller
         try {
             $room = RoomRepository::getRoomById($id);
         } catch (Exception $e) {
-            return $this->render("room/detail.html.twig", [
-                "message" => "Fehler beim Laden des Raums"
-            ]);
+            return $this->renderError("room_detail", $e);
         }
 
         if ($req->getMethod() === "GET") {
@@ -84,9 +77,7 @@ class RoomController extends Controller
             try {
                 RoomRepository::updateRoom($room);
             } catch (Exception $e) {
-                return $this->render("room/detail.html.twig", [
-                   "message" => "Fehler beim Speichern der Änderungen"
-                ]);
+                return $this->renderError("room_detail", $e);
             }
             return $this->render("room/detail.html.twig", [
                 "room" => $room,
