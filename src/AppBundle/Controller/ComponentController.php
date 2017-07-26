e0457dd17a6517515fe966d4c6fd20fb330df5c5
Thomas Schäfer <t.schaefer@headtrip.eu>
2017-07-26 14:34:50 +0200

    added creating, editing, and deleting components (no component types)

--- src/AppBundle/Controller/ComponentController.php
+++ src/AppBundle/Controller/ComponentController.php
@@ -3,13 +3,16 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Component;
+use AppBundle\Entity\ComponentType;
 use AppBundle\Entity\Repository\ComponentRepository;
 use AppBundle\Entity\Repository\ComponentTypeRepository;
 use AppBundle\Entity\Repository\RoomRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ComponentController extends Controller
 {
@@ -19,9 +22,16 @@ class ComponentController extends Controller
      *
      * @Route("/component", name="component_index")
      */
-    public function indexAction(Request $req)
+    public function indexAction(Request $req, SessionInterface $session)
     {
-
+        $message = $session->get('message');
+        $session->remove('message');
+        //get all components
+        $components = ComponentRepository::getAllComponents();
+        return $this->render("component/index.html.twig", array(
+            "components" => $components,
+            "message" => $message
+        ));
     }
 
     /**
@@ -75,7 +85,21 @@ class ComponentController extends Controller
             $component->setNote($req->get("note"));
             // set producer
             $component->setProducer($req->get("producer"));
-            //TODO: set component types
+            $component->setSupplierId(0);
+            //component type
+            $type_id = $req->get("type_id");
+            //check if component type with this id exists
+            $type = null;
+            try{
+                $type = ComponentTypeRepository::getComponentTypeById($type_id);
+            }catch(Exception $ex){
+                $error = "Es gab einen Fehlern beim Laden der Komponententypen";
+            }
+            if(!$type){
+                $error = "Komponententyp existiert nicht";
+            }else{
+                $component->setComponentTypeId($type_id);
+            }
             if(!$error){
                 //create component object
                 try{
@@ -95,10 +119,22 @@ class ComponentController extends Controller
         }
         //if method is GET -> render template
         else{
-            //get component types
-            $types = ComponentTypeRepository::getAllComponentTypes();
+            $rooms = array();
+            $types = array();
+            $error = false;
+            try{
+                //get rooms
+                $rooms = RoomRepository::getAllRooms();
+                //get component types
+                $types = ComponentTypeRepository::getAllComponentTypes();
+
+            }catch(Exception $exception){
+                $error = "Es gab einen Fehler beim Datenbankzugriff";
+            }
             return $this->render("component/create.html.twig", array(
-                "types" => $types
+                "rooms" => $rooms,
+                "types" => $types,
+                "messsage" => $error,       //TODO: meldungen verbessern
             ));
         }
     }
@@ -110,12 +146,49 @@ class ComponentController extends Controller
      *
      * @Route("/component/{id}", name="component_edit", requirements={"id": "\d+"})
      */
-    public function editAction($id, Request $req)
+    public function editAction($id, Request $req, SessionInterface $session)
     {
+        //get error message from session
+        $message = $session->get('message');
+        $session->remove('message');
         //get component by id
-        $component = null;
+        $component = ComponentRepository::getComponentById($id);
+        //get rooms
+        $rooms = RoomRepository::getAllRooms();
+        //get types
+        $types = ComponentTypeRepository::getAllComponentTypes();
         return $this->render("component/edit.html.twig", array(
             "component" => $component,
+            "rooms" => $rooms,
+            "types" => $types,
+            "message" => $message
         ));
+    }
+
+    /**
+     * @Route("/component/{id}/delete", name="component_delete", requirements={"id": "\d+"})
+     */
+    public function deleteAction($id, Request $request, SessionInterface $session){
+        //check if method is post
+
+        //get component by id
+        $component = ComponentRepository::getComponentById($id);
+        if(!$component){
+            return $this->createNotFoundException("Komponenten wurde nicht gefunden");
+        }else{
+            //check if component can be deleted
+            //TODO: this
+            if(true){
+                ComponentRepository::deleteComponentById($component->getId());
+                $session->set('message', 'Komponente wurde gelöscht');
+                return $this->redirectToRoute('component_index');
+            }else{
+                //TODO: get error why component can't be created
+                $error = "Grund hier";
+                $session->set('message', "Komponente konnte nicht gelöscht werden: $error");
+                return $this->redirect($request->headers->get('referer'));
+            }
+        }
+
     }
 }
