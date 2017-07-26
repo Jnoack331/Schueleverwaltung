f5db5c2f0b91542a69d881c4100a02c1ff951de1
obihoernchenxD <codhater2000@gmail.com>
2017-07-26 08:04:59 +0200

    Errorhandling im RoomController implementiert

--- src/AppBundle/Controller/RoomController.php
+++ src/AppBundle/Controller/RoomController.php
@@ -6,18 +6,25 @@ use AppBundle\Entity\Repository\RoomRepository;
 use AppBundle\Entity\Room;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 
-class RoomController extends Controller
-{
+class RoomController extends Controller {
     /**
      * Fetches all Rooms from the database and renders a template to display them
      *
      * @Route("/room", name="room_index")
      */
     public function indexAction(Request $req) {
-        $rooms = RoomRepository::getAllRooms();
-        $this->render("room_view", ["rooms" => $rooms]);
+        try {
+            $rooms = RoomRepository::getAllRooms();
+        } catch (Exception $e) {
+            return $this->render("room_view", [
+               "message" => "Fehler beim Laden der Räume"
+            ]);
+        }
+
+        return $this->render("room_view", ["rooms" => $rooms]);
     }
 
     /**
@@ -27,12 +34,24 @@ class RoomController extends Controller
      * @Route("/room/create", name="room_create")
      */
     public function createAction(Request $req) {
-        $room = new Room();
-        $room->setNumber($req->get("number"));
-        $room->setDescription($req->get("description"));
-        $room->setNote($req->get("note"));
+        if ($req->getMethod() === "GET") {
+            return $this->render("room_create", []);
+        } else {
+            $room = new Room();
+            $room->setNumber($req->get("number"));
+            $room->setDescription($req->get("description"));
+            $room->setNote($req->get("note"));
 
-        RoomRepository::createRoom($room);
+            try {
+                $id = RoomRepository::createRoom($room);
+            } catch (Exception $e) {
+                return $this->render("room_create", [
+                    "message" => "Fehler beim Erstellen des Raums"
+                ]);
+            }
+
+            return $this->redirectToRoute("room_detail", ["id" => $id]);
+        }
     }
 
     /**
@@ -43,11 +62,17 @@ class RoomController extends Controller
      * @Route("/room/{id}", name="room_detail", requirements={"id": "\d+"})
      */
     public function detailAction($id, Request $req) {
-        $room = RoomRepository::getRoomById($id);
+        try {
+            $room = RoomRepository::getRoomById($id);
+        } catch (Exception $e) {
+            return $this->render("room_detail", [
+                "message" => "Fehler beim Laden des Raums"
+            ]);
+        }
 
         if ($req->getMethod() === "GET") {
             // Show the room with $id
-            $this->render("room_detail_view", [
+            return $this->render("room_detail", [
                 "number"      => $room->getNumber(),
                 "description" => $room->getDescription(),
                 "note"        => $room->getNote()
@@ -58,7 +83,13 @@ class RoomController extends Controller
             $room->setDescription($req->get("description"));
             $room->setNote($req->get("note"));
 
-            RoomRepository::updateRoom($room);
+            try {
+                $id = RoomRepository::updateRoom($room);
+            } catch (Exception $e) {
+                return $this->render("room_detail", [
+                   "message" => "Fehler beim Speichern der Änderungen"
+                ]);
+            }
         }
     }
 }
