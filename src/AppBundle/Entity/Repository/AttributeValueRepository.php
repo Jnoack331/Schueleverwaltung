24aa8c02c372e035ce79ccbeeae2f01116724a74
Thomas Schäfer <t.schaefer@headtrip.eu>
2017-07-26 19:58:34 +0200

    Added Attribute and AttributeValue Repositories. started work on attribut values for component

---
+++ src/AppBundle/Entity/Repository/AttributeValueRepository.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Colin
+ * Date: 25.07.2017
+ * Time: 10:32
+ *
+ * Provides functions to modify component entities in
+ * the database.
+ *
+ */
+
+namespace AppBundle\Entity\Repository;
+
+
+use AppBundle\Entity\ManagedConnection;
+
+class AttributeValueRepository
+{
+
+    public static function getAttributeValueById($id){#
+
+    }
+
+    public function getAttributeValuesByComponentId($componentId){
+
+    }
+
+}
