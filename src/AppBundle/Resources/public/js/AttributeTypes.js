ea0ae7555a8ddbf1c19fbd12bad4dfddcefade0b
Julius Noack <noack@solutiondrive.de>
2017-07-27 16:46:20 +0200

    Hotfix

--- src/AppBundle/Resources/public/js/AttributeTypes.js
+++ src/AppBundle/Resources/public/js/AttributeTypes.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         $('#valueWrapper').append('' +
             '              <dl>\n' +
             '                    <dt>\n' +
-            '                        <label for="k_attributwert{{ loop.index0 }}">Attributname</label>\n' +
+            '                        <label for="k_attributwert">Attributname</label>\n' +
             '                    </dt>\n' +
             '                    <dd>\n' +
             '                        <input name="attributevalues[][name]" id="k_attributwert">\n' +
