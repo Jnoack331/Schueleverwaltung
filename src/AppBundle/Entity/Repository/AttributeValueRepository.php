f542a88fee32517cdce54076717b3be19647a47a
Thomas Schäfer <t.schaefer@headtrip.eu>
2017-07-27 08:07:33 +0200

    added repo and stuff for attribute value

--- src/AppBundle/Entity/Repository/AttributeValueRepository.php
+++ src/AppBundle/Entity/Repository/AttributeValueRepository.php
@@ -13,17 +13,56 @@
 namespace AppBundle\Entity\Repository;
 
 
+use AppBundle\Entity\AttributeValue;
 use AppBundle\Entity\ManagedConnection;
 
 class AttributeValueRepository
 {
 
-    public static function getAttributeValueById($id){#
+    /**
+     * @param $component_id
+     * @param $attribute_id
+     * @return AttributeValue
+     */
+    public static function getAttributeValue($component_id, $attribute_id){
+        return new AttributeValue();
+    }
+
+    public static function getAttributeValuesByComponentId($componentId){
 
     }
 
-    public function getAttributeValuesByComponentId($componentId){
+    public static function createAttributeValue($attributeValue){
 
+    }
+
+    /**
+     * @param AttributeValue $attribute_value
+     * @throws \Exception
+     */
+    public static function deleteAttributeValue($attributeValue){
+        $managedConnection = new ManagedConnection();
+        $connection = $managedConnection->getConnection();
+
+        $query = $connection->prepare("DELETE FROM komponente_hat_attribute WHERE komponenten_k_id = ? AND komponentenattribute_kat_id = ?;");
+
+        $k_id = 0;
+        $kat_id = 0;
+
+        $query->bind_param("ii", $k_id, $kat_id);
+
+        $k_id = $attributeValue->getId();
+        $kat_id = $attributeValue->getAttributeId();
+
+        $query->execute();
+
+        if($query->error)
+        {
+            $query->close();
+            throw new \Exception("Löschen des Attributes fehlgeschlagen");
+        }
+
+        $query->close();
     }
 
 }
